5860296661a793d9b5f14f9a748c0464524ec9f4
Nikita Tsukanov <keks9n@gmail.com>
2021-04-26 10:02:20 +0300

    Migrate

--- QuartierLatin.Backend/Database/Migrations/University/AddToUniversitySpecialitiesCostToAndCostFromColum.cs
+++ QuartierLatin.Backend/Database/Migrations/University/AddToUniversitySpecialitiesCostToAndCostFromColum.cs
@@ -3,15 +3,23 @@
 namespace QuartierLatin.Backend.Database.Migrations.University
 {
     [MigrationDate(2021, 4, 21, 13, 24)]
-    public class AddToUniversitySpecialitiesCostToAndCostFromColum : AutoReversingMigration
+    public class AddToUniversitySpecialitiesCostToAndCostFromColum : Migration
     {
         public override void Up()
         {
-            Alter.Table("UniversitySpecialties")
-                .AddColumn("CostFrom").AsInt32()
-                .AddColumn("CostTo").AsInt32();
+            Rename.Column("Cost").OnTable("UniversitySpecialties").To("CostFrom");
 
-            Delete.Column("Cost").FromTable("UniversitySpecialties");
+            Alter.Table("UniversitySpecialties")
+                .AddColumn("CostTo").AsInt32().Nullable();
+
+            Execute.Sql("UPDATE \"UniversitySpecialties\" SET \"CostTo\" = \"CostFrom\"");
+            Alter.Column("CostTo").OnTable("UniversitySpecialties").AsInt32().NotNullable();
+
+        }
+
+        public override void Down()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
