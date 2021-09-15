d3bcaeae61b854f509142b558bd7803020b0a6f7
Nikita Emelin <clever899@gmail.com>
2021-09-15 19:21:05 +0300

    Add name to accommodation dto

--- QuartierLatin.Backend/Dto/HousingCatalogDto/RouteDto/HousingAccommodationTypeModuleDto.cs
+++ QuartierLatin.Backend/Dto/HousingCatalogDto/RouteDto/HousingAccommodationTypeModuleDto.cs
@@ -4,6 +4,7 @@ namespace QuartierLatin.Backend.Dto.HousingCatalogDto.RouteDto
 {
     public class HousingAccommodationTypeModuleDto
     {
+        public string Name { get; set; }
         public string Square { get; set; }
         public string Residents { get; set; }
         public int Price { get; set; }
