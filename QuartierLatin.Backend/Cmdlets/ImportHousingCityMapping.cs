fe5e1b6e78a1027cca5a149a2c1a3ee08e6d198d
Kirill Poletaev <mr.dead.toast@gmail.com>
2021-11-29 13:35:54 +0300

    Housing to city bind fix

--- QuartierLatin.Backend/Cmdlets/ImportHousingCityMapping.cs
+++ QuartierLatin.Backend/Cmdlets/ImportHousingCityMapping.cs
@@ -10,6 +10,7 @@ using LinqToDB;
 using Microsoft.Extensions.Configuration;
 using QuartierLatin.Backend.Application.ApplicationCore.Interfaces.Repositories;
 using QuartierLatin.Backend.Application.ApplicationCore.Models.CatalogModels;
+using QuartierLatin.Backend.Application.ApplicationCore.Models.HousingModels;
 using QuartierLatin.Backend.Application.Infrastructure.Database;
 using QuartierLatin.Backend.Application.Infrastructure.Database.AppDbContextSeed;
 using QuartierLatin.Backend.Config;
@@ -74,14 +75,13 @@ namespace QuartierLatin.Backend.Cmdlets
             {
                 var dbCities = await db.CommonTraits.Where(x => x.CommonTraitTypeId == 2).ToListAsync();
                 // OldId -> NewId
-                var oldCityToCityMap = new Dictionary<int, int>();
-                dump.Cities.ForEach(x =>
+                var oldCityToCityMap = dump.Cities.Aggregate(new Dictionary<int, int>(), (acc, x) =>
                 {
                     var city = dbCities.FirstOrDefault(c => c.Names.ContainsValue(x.Name));
                     if (city is not null)
                     {
-                        oldCityToCityMap[x.Id] = city.Id;
-                        return;
+                        acc[x.Id] = city.Id;
+                        return acc;
                     }
                     
                     Console.WriteLine($"Missing {x.Name} at database! Inserting...");
@@ -89,7 +89,8 @@ namespace QuartierLatin.Backend.Cmdlets
                     var id = db.InsertWithInt32Identity(newCity);
                     newCity.Id = id;
                     dbCities.Add(newCity);
-                    oldCityToCityMap[x.Id] = id;
+                    acc[x.Id] = id;
+                    return acc;
                 });
                 
                 Console.WriteLine("Creating parent links between CommonTraits");
@@ -103,7 +104,48 @@ namespace QuartierLatin.Backend.Cmdlets
                 });
                 
                 Console.WriteLine("Fixing missing housing cities");
-                Console.WriteLine("TODO!");
+                var urlPaths = dump.Housings.Select(x => x.Name.Replace(" ", "-")).ToList();
+                var housings = await db.HousingLanguages
+                    .Where(x => urlPaths.Contains(x.Url) && x.LanguageId == 2)
+                    .ToListAsync();
+
+
+                // Fix naming to simplify querying via our housings
+                var housingsFixedUrl = dump.Housings.Select(x =>
+                {
+                    x.Name = x.Name.Replace(" ", "-");
+                    return x;
+                }).ToList();
+                
+                var housingsCount = housings.Count;
+                var progress = 0;
+                void ReportProgress() => Console.WriteLine($"{++progress}/{housingsCount} processed");
+                
+                housings.ForEach(x =>
+                {
+                    var oldHousing = housingsFixedUrl.First(d => d.Name == x.Url);
+                    var hasCity = (from traits in db.CommonTraits
+                        join hTraits in db.CommonTraitToHousings on traits.Id equals hTraits.CommonTraitId
+                        join housingLanguage in db.HousingLanguages on hTraits.HousingId equals housingLanguage
+                            .HousingId
+                        where housingLanguage.Url == oldHousing.Name && traits.CommonTraitTypeId == 2
+                        select traits).FirstOrDefault();
+                    
+                    if (hasCity is not null)
+                    {
+                        ReportProgress();
+                        return;
+                    };
+                    
+                    Console.WriteLine($"Fixing: {x.Url}");
+                    db.Insert(new CommonTraitToHousing
+                    {
+                        HousingId = x.HousingId,
+                        CommonTraitId = oldCityToCityMap[oldHousing.CityId]
+                    });
+                    ReportProgress();
+                });
+                
             });
             return 0;
         }
