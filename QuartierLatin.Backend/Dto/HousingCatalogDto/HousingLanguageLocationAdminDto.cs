c6ee8a619b0cad261ae9a4170614582322cb06ee
Alexei Schukin <alexus211@yandex.ru>
2021-12-21 01:08:43 +0300

    coordinates adding to housing via admin panel BACKEND

---
+++ QuartierLatin.Backend/Dto/HousingCatalogDto/HousingLanguageLocationAdminDto.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using RemoteUi;
+
+namespace QuartierLatin.Backend.Dto.HousingCatalogDto
+{
+    public class HousingLanguageLocationAdminDto
+    {
+
+        [RemoteUiField("Lat")]
+		public string Lat { get; set; }
+        [RemoteUiField("Lng")]
+		public string Lng { get; set; }
+        [RemoteUiField("Address")]		
+		public string Address { get; set; }
+
+    }
+}
