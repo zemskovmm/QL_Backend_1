c060eeab98c75ffff786ef26366a25ca19e3c41a
Alexei Schukin <alexus211@yandex.ru>
2021-12-21 10:28:24 +0300

    location and address adding via admin panel for housing for autogeneration Part 3

--- QuartierLatin.Backend/Dto/HousingCatalogDto/HousingLanguageAdminDto.cs
+++ QuartierLatin.Backend/Dto/HousingCatalogDto/HousingLanguageAdminDto.cs
@@ -11,12 +11,7 @@ namespace QuartierLatin.Backend.Dto.HousingCatalogDto
         public string HtmlDescription { get; set; }
         [RemoteUiField("Url")]
         public string Url { get; set; }
-		[RemoteUiField("Lat")]
-		public string Lat { get; set; }
-        [RemoteUiField("Lng")]
-		public string Lng { get; set; }
-        [RemoteUiField("Address")]		
-		public string Address { get; set; }
+
 		
         public JObject? Metadata { get; set; }
         public HousingLanguageLocationAdminDto Location { get; set; }
